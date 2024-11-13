cee1a0233de7cd50dd896859dc0dc43150f29fd0
Omer1015 <omerelbashir15@gmail.com>
2024-11-13 22:53:45 +0400

    implemet temporary class data

---
+++ js/Classes.js
@@ -0,0 +1,12 @@
+let products = [
+    {
+        id: 1001,
+        title: "Quadratic Equations",
+        description: "Learn about quadratic equations",
+        price: 60,
+        image: "./img/quadtraticform.jpg",
+        availableInventory: 5,
+        Subject: "Math",
+        Location: "Grove",
+    },
+];
